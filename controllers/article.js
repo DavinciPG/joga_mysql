2735a41ce4f2286e1c35203c8b2ec1fe4082ac50
Erki <erkiparna7@gmail.com>
2022-12-08 15:00:06 +0200

    Created article controller and route, refactored application code + Created author controller and route, refactored application code

---
+++ controllers/article.js
@@ -0,0 +1,45 @@
+const con = require('../utils/db');
+
+const getAllArticles = (req, res) => {
+    let query = "SELECT * FROM article"
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+}
+const getArticleBySlug = (req, res) => {
+    let query = `SELECT article.id, article.name, article.slug, article.image, article.body, article.published, author.name AS author, author.id AS author_id FROM article JOIN author ON article.author_id = author.id WHERE slug = "${req.params.slug}";`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err
+        article = result
+        res.render('article', {
+            article: article
+        })
+    })
+}
+
+const getArticlesByAuthor = (req, res) => {
+    let query = `SELECT article.id, article.name, article.slug, article.image, article.body, article.published, author.name AS author, author.id AS author_id FROM article JOIN author ON article.author_id = author.id WHERE author_id = "${req.params.author_id}";`
+    let articles = []
+    let author
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        author = result[0]
+        res.render('author', {
+            articles: articles,
+            author: author
+        })
+    })
+}
+
+module.exports = {
+    getAllArticles,
+    getArticleBySlug,
+    getArticlesByAuthor
+}
--- index.js
+++ index.js
@@ -18,62 +18,14 @@ app.engine('hbs', hbs.engine({
 // public directory
 app.use(express.static('public'));
 
-const mysql = require('mysql');
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-// connection
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'qwerty',
-    database: 'joga_mysql'
-});
+const articleRoutes = require('./routes/article');
 
-con.connect(function(err) {
-   if(err) throw err;
-   console.log('Connection stable.');
-});
-
-app.get('/', (req, res) => {
-    let query = 'SELECT * FROM article';
-    let articles = [];
-    con.query(query, (err, result) => {
-       if(err) throw err;
-       articles = result;
-       res.render('index', {
-        articles: articles
-       });
-    });
-});
-
-app.get('/author/:author_id', (req, res) => {
-    let query = `select article.id, article.name, article.slug, article.image, article.body, article.published, author.name as author, author.id as author_id from article JOIN author ON article.author_id = author.id where author_id = "${req.params.author_id}";`
-    let articles = []
-    let author
-    con.query(query, (err, result) => {
-        if (err) throw err
-        articles = result
-        author = result[0]
-        res.render('author', {
-            articles: articles,
-            author: author
-        })
-    })
-});
-
-app.get('/article/:slug', (req, res) => {
-    let query = `SELECT article.id, article.name, article.slug, article.image, article.body, article.published, author.name AS author, author.id AS author_id FROM article JOIN author ON article.author_id = author.id WHERE slug = "${req.params.slug}";`
-    let article
-    con.query(query, (err, result) => {
-       if(err) throw err;
-       article = result;
-       res.render('article', {
-           article: article
-       });
-    });
-});
+app.use('/', articleRoutes);
+app.use('/article', articleRoutes);
+app.use('/author', articleRoutes);
 
 // start app
 app.listen(3000, () => {
