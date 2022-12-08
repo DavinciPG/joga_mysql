7f47da05beee3befba4eb21a919619eb97fe5519
Erki <erkiparna7@gmail.com>
2022-12-08 11:46:08 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -48,6 +48,18 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/article/:slug', (req, res) => {
+ let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`;
+    let article
+    con.query(query, (err, result) => {
+       if(err) throw err;
+       article = result;
+       res.render('article', {
+        article: article
+       });
+    });
+});
+
 // start app
 app.listen(3000, () => {
    console.log('App listening at 3000');
